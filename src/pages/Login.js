f326e58884116f99ac55e30b0d3d3546b448bc00
박준영 <skyzuni@nate.com>
2021-08-02 14:33:52 +0900

    Remove: 불필요한 코드 삭제

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Image, Text } from "../elements";
+import { Text } from "../elements";
 
 const Login = () => {
   return (
