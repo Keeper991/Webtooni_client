3f5a99583d5e99dd3feea49d5b86d721f71a2584
Sehyeon Park <nesmile23@gmail.com>
2021-08-02 15:47:08 +0900

    Refactor: Slick 추가

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import WebToonCard from "./WebToonCard";
 import ReviewCard from "./ReviewCard";
 import BestReveiwerCard from "./BestReviewerCard";
 import WebToonMonth from "./WebToonMonth";
+import Slick from "./Slick";
 
-export { WebToonCard, ReviewCard, BestReveiwerCard, WebToonMonth };
+export { WebToonCard, ReviewCard, BestReveiwerCard, WebToonMonth, Slick };
