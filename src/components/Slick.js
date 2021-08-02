975606e2a205851407f5885b83fe0084b680857b
junyp1 <85473148+junyp1@users.noreply.github.com>
2021-08-03 02:27:43 +0900

    Feat: 슬라이더 기능 추가 (#17)
    * Feat: 슬라이더 기능 추가
    
    * Style: Slick props에 number type 수정
    
    "" => {}
    
    Co-authored-by: bandor <57912049+Keeper991@users.noreply.github.com>

--- src/components/Slick.js
+++ src/components/Slick.js
@@ -5,9 +5,75 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 // children에 width나 display 속성을 사용할 경우, !important를 붙여줄 것.
-const Slick = ({ width, children, ...props }) => {
+const Slick = ({
+  width,
+  children,
+  is_arrow,
+  is_variableWidth,
+  is_infinite,
+  ...props
+}) => {
+  const CustomNextArrows = (props) => {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{
+          ...style,
+          display: "block",
+          background: "#ccc",
+          position: "absolute",
+          right: "10%",
+          top: "5%",
+        }}
+        onClick={onClick}
+      ></div>
+    );
+  };
+
+  const CustomPrevArrows = (props) => {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{
+          ...style,
+          display: "block",
+          background: "#ccc",
+          position: "absolute",
+          left: "10%",
+          top: "5%",
+          zIndex: 1,
+        }}
+        onClick={onClick}
+      ></div>
+    );
+  };
+
+  if (is_arrow) {
+    return (
+      <SliderWrap
+        infinite={is_infinite}
+        speed={300}
+        slidesToShow={1}
+        draggable
+        variableWidth={is_variableWidth}
+        nextArrow={<CustomNextArrows></CustomNextArrows>}
+        prevArrow={<CustomPrevArrows></CustomPrevArrows>}
+      >
+        {children}
+      </SliderWrap>
+    );
+  }
+
   return (
-    <SliderWrap infinite speed="300" slidesToShow="1" draggable variableWidth>
+    <SliderWrap
+      infinite={is_infinite}
+      speed={300}
+      slidesToShow={1}
+      draggable
+      variableWidth={is_variableWidth}
+    >
       {children}
     </SliderWrap>
   );
@@ -15,6 +81,8 @@ const Slick = ({ width, children, ...props }) => {
 
 Slick.defaultProps = {
   width: "90%",
+  is_infinite: false,
+  is_variableWidth: true,
 };
 
 const SliderWrap = styled(Slider)`
