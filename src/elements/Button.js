c73ca10aacd422542fe2f16d033ad0dd46a7b50f
Sehyeon Park <nesmile23@gmail.com>
2021-08-05 01:39:34 +0900

    Feat: Button Shape 및 disabled 속성 추가

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -16,6 +16,9 @@ const Button = (props) => {
     borderRadius,
     fontWeight,
     border,
+    shape,
+    size,
+    disabled,
   } = props;
 
   const styles = {
@@ -29,7 +32,29 @@ const Button = (props) => {
     borderRadius: borderRadius,
     fontWeight: fontWeight,
     border: border,
+    size: size,
+    disabled: disabled,
   };
+
+  if (shape === "circle") {
+    return (
+      <React.Fragment>
+        <ElCircleButton {...styles} onClick={_onClick}>
+          {children}
+        </ElCircleButton>
+      </React.Fragment>
+    );
+  }
+
+  if (shape === "pill") {
+    return (
+      <React.Fragment>
+        <ElPillButton {...styles} onClick={_onClick}>
+          {children}
+        </ElPillButton>
+      </React.Fragment>
+    );
+  }
 
   return (
     <React.Fragment>
@@ -53,7 +78,58 @@ Button.defaultProps = {
   borderRadius: "4px",
   fontWeight: "normal",
   border: `1px solid ${Color.darkGray}`,
+  shape: "",
+  size: 0,
+  disabled: false,
 };
+
+const ElCircleButton = styled.button`
+  width: ${(props) => props.size};
+  height: ${(props) => props.size};
+  margin: ${(props) => props.margin};
+  color: ${(props) => props.color};
+  background-color: ${(props) => props.bgColor};
+  font-size: ${(props) => props.fontSize};
+  font-weight: ${(props) => props.fontWeight};
+  border: ${(props) => props.border};
+  border-radius: 100%;
+
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+
+  &:focus,
+  &:active {
+    outline: none;
+  }
+`;
+
+const ElPillButton = styled.button`
+  width: ${(props) => props.width};
+  padding: ${(props) => props.padding};
+  margin: ${(props) => props.margin};
+  color: ${(props) => props.color};
+  background-color: ${(props) => props.bgColor};
+  font-size: ${(props) => props.fontSize};
+  font-weight: ${(props) => props.fontWeight};
+  border: ${(props) => props.border};
+  border-radius: calc(
+    ${(props) => parseInt(props.padding.split(" ")[0]) * 2}px + 1em
+  );
+
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+
+  &:focus,
+  &:active {
+    outline: none;
+  }
+`;
 
 const ElButton = styled.button`
   width: ${(props) => props.width};
@@ -72,6 +148,10 @@ const ElButton = styled.button`
   justify-content: center;
   align-items: center;
   white-space: nowrap;
+
+  ${(props) =>
+    props.disabled &&
+    `background-color: ${Color.lightGray}; color: ${Color.gray}; border: 1px solid transparent; cursor: not-allowed;`}
 
   &:focus,
   &:active {
