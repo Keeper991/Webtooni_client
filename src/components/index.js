5c0ad67d9a10c70e1b80ae390896a792ad4d149b
박준영 <skyzuni@nate.com>
2021-08-04 14:43:33 +0900

    Feat: 추천 페이지 기능 추가

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,14 @@ import WebToonCard from "./WebToonCard";
 import ReviewCard from "./ReviewCard";
 import BestReveiwerCard from "./BestReviewerCard";
 import WebToonMonth from "./WebToonMonth";
+import OfferCard from "./OfferCard";
 import Slick from "./Slick";
 
-export { WebToonCard, ReviewCard, BestReveiwerCard, WebToonMonth, Slick };
+export {
+  WebToonCard,
+  ReviewCard,
+  BestReveiwerCard,
+  WebToonMonth,
+  OfferCard,
+  Slick,
+};
