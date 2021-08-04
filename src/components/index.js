b2cc1971341c7621ca0c3131fcaee44f0a72adf0
LAPPY\은혜 <elivevvv@gmail.com>
2021-08-04 15:50:54 +0900

    Feat: 별점 컴포넌트 index.js에 추가

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,17 @@
 import WebToonCard from "./WebToonCard";
 import ReviewCard from "./ReviewCard";
 import BestReveiwerCard from "./BestReviewerCard";
-import DetailReview from "./DetailReview";
 import WebToonMonth from "./WebToonMonth";
 import Slick from "./Slick";
+import DetailReview from "./DetailReview";
+import DetailStar from "./DetailStar";
 
-export { WebToonCard, ReviewCard, BestReveiwerCard, WebToonMonth, Slick, DetailReview };
+export {
+  WebToonCard,
+  ReviewCard,
+  BestReveiwerCard,
+  WebToonMonth,
+  Slick,
+  DetailReview,
+  DetailStar,
+};
