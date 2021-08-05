59b15cca57e825d0f013bbbb186b4648c5d1e762
LAPPY\은혜 <elivevvv@gmail.com>
2021-08-05 13:38:00 +0900

    Remove: Board 페이지 삭제, Feat: Talk 메인,작성,상세 페이지 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,22 @@ import Main from "./Main";
 import Recommendation from "./Recommendation";
 import Review from "./Review";
 import Detail from "./Detail";
-import Board from "./Board";
 import Search from "./Search";
 import MyPage from "./MyPage";
 import Login from "./Login";
+import Talk from "./Talk";
+import TalkWrite from "./TalkWrite";
+import TalkDetail from "./TalkDetail";
 
-export { Main, Recommendation, Review, Detail, Board, Search, MyPage, Login };
+export {
+  Main,
+  Recommendation,
+  Review,
+  Detail,
+  Search,
+  MyPage,
+  Login,
+  Talk,
+  TalkWrite,
+  TalkDetail,
+};
