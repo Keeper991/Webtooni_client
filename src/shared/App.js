935a5df62419637998dfd218c4175e5496c4fac8
bandor <57912049+Keeper991@users.noreply.github.com>
2021-08-14 16:08:47 +0900

    Feat: web font 적용 | 별점 및 구독 기능 추가 | 기타 버그 수정 (#60)
    * Remove: Font Files
    
    * Design: Pill button에 padding props 적용
    
    * Feat: 별점 디자인 및 기능 수정
    
    * Design: a 태그 전역 디자인 수정
    
    * Fix: Modal component 추가
    
    * Feat: subscribe 기능 추가 및 별점 기능 추가

--- src/shared/App.js
+++ src/shared/App.js
@@ -35,28 +35,31 @@ function App() {
   }, []);
 
   return (
-    <ConnectedRouter history={history}>
-      <GlobalStyle></GlobalStyle>
-      <Header></Header>
-      <Route path="/" exact component={Main} />
-      <Route path="/detail/:id" exact component={Detail} />
-      <Route path="/toonlist/:id" exact component={ToonList} />
-      <Route path="/login" exact component={Login} />
-      <Route path="/taste" exact component={Taste} />
-      <Route path="/profile" exact component={Profile} />
-      <Route path="/recommendation" exact component={Recommendation} />
-      <Route path="/review" exact component={Review} />
-      <Route path="/review/search" exact component={ReviewSearch} />
-      <Route path="/review/write/:id" exact component={ReviewSearchWrite} />
-      <Route path="/search" exact component={Search} />
-      <Route path="/mypage" exact component={MyPage} />
-      <Route path="/talk" exact component={Talk} />
-      <Route path="/talk/write" exact component={TalkWrite} />
-      <Route path="/talk/write/:id" exact component={TalkWrite} />
-      <Route path="/talk/detail/:id" exact component={TalkDetail} />
-      <Route path="/user/kakao" exact component={KakaoLogin} />
-      <Route path="/user/naver" exact component={NaverLogin} />
-    </ConnectedRouter>
+    <>
+      <ConnectedRouter history={history}>
+        <GlobalStyle></GlobalStyle>
+        <Header></Header>
+        <Route path="/" exact component={Main} />
+        <Route path="/detail/:id" exact component={Detail} />
+        <Route path="/toonlist/:id" exact component={ToonList} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/taste" exact component={Taste} />
+        <Route path="/profile" exact component={Profile} />
+        <Route path="/recommendation" exact component={Recommendation} />
+        <Route path="/review" exact component={Review} />
+        <Route path="/review/search" exact component={ReviewSearch} />
+        <Route path="/review/write/:id" exact component={ReviewSearchWrite} />
+        <Route path="/search" exact component={Search} />
+        <Route path="/mypage" exact component={MyPage} />
+        <Route path="/talk" exact component={Talk} />
+        <Route path="/talk/write" exact component={TalkWrite} />
+        <Route path="/talk/write/:id" exact component={TalkWrite} />
+        <Route path="/talk/detail/:id" exact component={TalkDetail} />
+        <Route path="/user/kakao" exact component={KakaoLogin} />
+        <Route path="/user/naver" exact component={NaverLogin} />
+      </ConnectedRouter>
+      <Modal />
+    </>
   );
 }
 
