86a2ddb962f0295a5679de5536dbc2c4af5c6333
Sehyeon Park <nesmile23@gmail.com>
2021-08-20 03:00:42 +0900

    Fix: 별점 표시되지 않는 문제 수정

--- src/pages/ReviewWrite.js
+++ src/pages/ReviewWrite.js
@@ -141,15 +141,15 @@ const ReviewWrite = (props) => {
 
       <Grid padding="20px 20px 15px">
         <DetailStar
+          starPoint={starPoint}
           onStarClick={(starPoint) => {
             if (!loading && is_login) {
               setStarPoint(starPoint);
               dispatch(reviewActions.putStarServer(webtoon_id, starPoint));
-              starPoint = { starPoint };
             } else if (loading && is_login) {
-              alert('응답 대기중입니다. 잠시만 기다려주세요.')
+              alert("응답 대기중입니다. 잠시만 기다려주세요.");
             } else {
-              alert('로그인이 필요한 서비스입니다. 로그인해주세요.')
+              alert("로그인이 필요한 서비스입니다. 로그인해주세요.");
             }
           }}
         />
