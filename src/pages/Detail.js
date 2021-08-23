bdc06ae11d861b9c34db92d804824005b6eb4826
박준영 <skyzuni@nate.com>
2021-08-23 20:02:23 +0900

    Fix: 상세페이지 웹툰 데이터 수정

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -464,7 +464,7 @@ const Detail = (props) => {
             <SliderBox>
               <CardSliderBox>
                 {similarToons.map((item, idx) => (
-                  <WebToonCard key={idx} {...item} id={item.toonId} />
+                  <WebToonCard key={idx} {...item} id={item.toonId} detail />
                 ))}
               </CardSliderBox>
             </SliderBox>
